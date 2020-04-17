3b77b00cf5dba3ee3dcd6311911b0674f98c8969
Administrator <1783157378@qq.com>
2020-04-17 16:55:44 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/cn/blb/test/Hello.java

--- src/main/java/cn/blb/test/Hello.java
+++ src/main/java/cn/blb/test/Hello.java
@@ -3,6 +3,6 @@ package cn.blb.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("weixins");
+        System.out.println("weixin");
     }
 }
